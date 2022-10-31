876cfc29da8294fe0f7cadfc0021d0b6c00318c9
CezaryNaskret <ceznas000@utp.edu.pl>
2022-10-31 20:38:35 +0100

    dodanie serwisu do wysyłanie email

---
+++ kerplank/src/main/kotlin/utp/agile/kerplank/domain/email/EmailController.kt
@@ -0,0 +1,25 @@
+package utp.agile.kerplank.domain.email
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/email")
+class EmailController(val emailService: EmailService) {
+
+    @GetMapping("/send")
+    fun sendEmail(): ResponseEntity<String> {
+        return ResponseEntity<String>(
+            emailService.sendEmail(
+                "kerplank.project@gmail.com",
+                "kerplank.project@gmail.com",
+                "testowy email",
+                "treść"
+            ),
+            HttpStatus.OK
+        )
+    }
+}
