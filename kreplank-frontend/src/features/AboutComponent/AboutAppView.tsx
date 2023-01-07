883d91d3d349b3496694f4beaaf6587e3d0d60aa
Paczaj <paczaj7@gmail.com>
2023-01-07 22:30:15 +0100

    Zmodyfikowanie strony: O plikacji

--- kreplank-frontend/src/features/AboutComponent/AboutAppView.tsx
+++ kreplank-frontend/src/features/AboutComponent/AboutAppView.tsx
@@ -1,5 +1,5 @@
 
-import { Box, Button } from '@mui/material'
+import { Box, Button, Container,List, ListItem, ListItemText, Typography } from '@mui/material'
 import { NextPage } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
@@ -14,9 +14,34 @@ const AboutAppView = () => {
 
     const router = useRouter()
 
-    return <Box >
-        about page
+    return (
+<Container component="main"
+           sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        }}
+>
+<Typography>
+<Box sx={{ fontWeight: 'bold', fontSize: 16 }}>
+Aplikacja Webowa do zarządzania projektami przy użyciu technik programowania zwinnego i spełnia następujące zadania:
+</Box>
+</Typography>
+    <Box>
+        <nav aria-label="elements list">
+        <List>
+                    <ListItemText primary="- utworzenie nowego użytkownika z odpowiednią rolą," />
+                    <ListItemText primary="- zarządzanie projektami oraz zadaniami przypisanymi do projektu," />
+                    <ListItemText primary="- możliwość przesyłania na serwer i pobierania plików z określonego projektu," />
+                    <ListItemText primary="- funkcjonalność systemu pozwalającą dodawać, modyfikować i usuwać dane projektów, zadań i studentów," />
+                    <ListItemText primary="- ogólnodostępny chat korzystający z dwukierunkowego kanału websocketowego," />
+                    <ListItemText primary="- transmisja danych szyfrowana protokołem." />
+        </List>
+        </nav>
     </Box>
+</Container>
+);
 }
 
 
