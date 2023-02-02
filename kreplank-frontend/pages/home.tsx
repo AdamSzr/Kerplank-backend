8b38451c7a48f501ae3ec54a6181e618456d89b3
CezaryNaskret <ceznas000@utp.edu.pl>
2023-02-02 20:32:48 +0100

    data formater

--- kreplank-frontend/pages/home.tsx
+++ kreplank-frontend/pages/home.tsx
@@ -1,10 +1,4 @@
-import { Box, Button } from '@mui/material'
 import { NextPage } from 'next'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { NextRequest, NextResponse } from 'next/server'
-import { useEffect } from 'react'
-import { backendUrlStorage } from '../src/features/config'
 import HomeViewComponent from '../src/features/AppRootComponent/AppRootViewComponent'
 
 
