0edbc54c1b94f809e9c764942cff860373fd1487
Adam Szreiber <adam.szr98@gmail.com>
2023-03-23 23:52:52 +0100

    Refactoring

--- kreplank-frontend/src/features/Projects/ProjectFileUploadComponent.tsx
+++ kreplank-frontend/src/features/Projects/ProjectFileUploadComponent.tsx
@@ -1,6 +1,7 @@
 
 
-import React, { FormEvent, useContext, useRef, useState } from 'react'
+import React, { FormEvent, useContext, useEffect, useRef, useState } from 'react'
+import { Stack } from '@mui/system'
 import { Box, Button, Container, Input, Typography } from '@mui/material'
 import { replaceItemInArray } from '../utils/ArrayUtils'
 import { Project } from '../models/Project'
@@ -12,26 +13,30 @@ import { ProjectViewContext } from './ProjectsComponent'
 
 const ProjectFileUploadComponent:React.FC<{ project?: Project}> = ({ project }) => {
   const [ selectedFile, setSelectedFile ] = useState<File | null>(null)
-
+  const [ dataForm, setDataForm ] = useState<FormData | null>(null)
   const ctx = useContext( ProjectViewContext )
 
   const inputRef = useRef<HTMLFormElement>()
 
-
-  function onUploadSuccess() {
-    console.log( `success` )
-  }
-    
-  const  uploadClick:React.FormEventHandler<HTMLFormElement> =  async function( event )  {
-
-    event.preventDefault()
-
-    const dataForm = new FormData( inputRef.current )
-    console.log( `FormData size --`, dataForm.getAll( `files` ).length )
-
-    dataForm.getAll( `files` ).forEach( it => {
+  useEffect( () => {
+    if (!dataForm) return 
+    dataForm!.getAll( `files` ).forEach( it => {
       console.log( (it as File).name )
     } )
+
+  }, [ dataForm != null ] )
+
+
+  function onUploadSuccess() {
+    clearFormData()
+  }
+
+  const  uploadClick:React.FormEventHandler<HTMLFormElement> =  async function( event )  {
+    event.preventDefault()
+
+    if (!dataForm) return
+    
+    console.log( `FormData items size --`, dataForm.getAll( `files` ).length )
 
     const { result, proj } = await uploadMultipleFile( dataForm, `/${project?.id}` ).then( it => ({ ...it, proj:it.project }) )
 
@@ -41,7 +46,7 @@ const ProjectFileUploadComponent:React.FC<{ project?: Project}> = ({ project }) 
       return
     }
 
-    console.log( project, `mf` )
+    // console.log( project )
 
     const newProjList = [ ctx.projectList!.slice( 0, idx ), proj, ctx.projectList!.slice( idx + 1 ) ]
     ctx.setProjectList( newProjList as Project[] )
@@ -50,7 +55,27 @@ const ProjectFileUploadComponent:React.FC<{ project?: Project}> = ({ project }) 
   }
 
 
+  const handleFileChange = (e:unknown) => {
+    console.log( `file added`, e )
+    const dataForm = new FormData( inputRef.current )
+    setDataForm( dataForm )
+  }
 
+
+  const ShowFiles = () => {
+    dataForm!.getAll( `files` ).forEach( it => {
+      console.log( (it as File).name )
+    } )
+    return <>{dataForm!.getAll( `files` ).filter( it => it instanceof File ).map( file => <Box>{(file as File).name}</Box> )}</>
+  }
+
+  const clearFormData = () => {
+    inputRef.current?.reset()
+    setDataForm( null ) 
+  }
+
+
+  console.log({ disabled:Boolean( !dataForm ) })
 
   return (
     <Container sx={
@@ -68,18 +93,34 @@ const ProjectFileUploadComponent:React.FC<{ project?: Project}> = ({ project }) 
       }
     }
     >
-      <Typography textAlign="center"> Uploading to /{project?.title}</Typography>
-      <Box>
+      <Typography textAlign="center" sx={{ marginBottom:`30px` }}> {dataForm == null ? `Wybierz plik aby przesłać` : `Twoje pliki:`}</Typography>
+
+       
+      {
+        dataForm != null &&
+        <Stack textAlign="center">
+          <ShowFiles />
+        </Stack>
+      }
+
+       
+      <Box display="flex" justifyContent="center">
         <form
           ref={inputRef as React.LegacyRef<HTMLFormElement>}
           onSubmit={uploadClick}
           method='post' encType="multipart/form-data"
         >
-          <Box>
-            <input type="file" name="files" multiple />
+          <Box sx={{ display:dataForm != null ? `none` : `unset` }}>
+            <input type="file" name="files" multiple onChange={e => { handleFileChange( e ) }}   />
           </Box>
-          <Box>
-            <Button type='submit'>Submit</Button>
+
+         
+
+          <Box sx={{ marginTop:`40px`, display:`flex`, gap:`5px` }}>
+            <Button
+              fullWidth variant="contained" onClick={() => clearFormData}
+            >Wyczyść</Button>
+            <Button fullWidth disabled={Boolean( !dataForm )} color="secondary" variant="contained" type='submit'>Zapisz</Button>
           </Box>
         </form>
       </Box>
